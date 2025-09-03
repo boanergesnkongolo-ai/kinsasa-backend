02469d141b1d7932eb406f679228302ac27a16c6
boanergesnkongolo-ai <boanergesnkongolo@gmail.com>
2025-09-03 14:42:53 +0100

    Delete routes directory

--- routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-// GET route simple
-router.get("/", (req, res) => {
-  res.send("Bienvenue sur la route des utilisateurs !");
-});
-
-module.exports = router;
